d73f1287b8179a2f9a2276a180281e66a44c7a34
Katsuking <legendsoccerforte5@gmail.com>
2024-10-06 19:18:04 +0900

    preventing directory traversal attack

--- http_server/scratch/src/website_handler.rs
+++ http_server/scratch/src/website_handler.rs
@@ -14,7 +14,18 @@ impl WebsiteHandler {
     pub fn read_file(&self, file_path: &str) -> Option<String> {
         let path = format!("{}/{}", self.public_path, file_path);
         // ok() is a super handy method to convert Result -> Option
-        fs::read_to_string(path).ok()
+        // canonicalize -> remove ../ in path
+        match fs::canonicalize(path) {
+            Ok(path) => {
+                if path.starts_with(&self.public_path) {
+                    fs::read_to_string(path).ok()
+                } else {
+                    println!("Directory traversal attack attempted: {}", file_path);
+                    None
+                }
+            }
+            Err(_) => None,
+        }
     }
 }
 
@@ -24,7 +35,11 @@ impl Handler for WebsiteHandler {
             Method::GET => match request.path() {
                 "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
                 "/hello" => Response::new(StatusCode::Ok, Some("<h1>Hello world</h1>".to_string())),
-                _ => Response::new(StatusCode::BadRequest, None),
+                // dealing with arbitrary files
+                path => match self.read_file(path) {
+                    Some(contents) => Response::new(StatusCode::Ok, Some(contents)),
+                    None => Response::new(StatusCode::NotFound, None),
+                },
             },
             _ => Response::new(StatusCode::BadRequest, None),
         }
