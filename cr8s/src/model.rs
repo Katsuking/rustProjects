74b7a1f23e22ec459c9b3c1a2b86cecc75f7353e
Katsuking <legendsoccerforte5@gmail.com>
2025-10-15 19:07:17 +0900

    feat: diesel create models

--- cr8s/src/model.rs
+++ cr8s/src/model.rs
@@ -1,1 +1,39 @@
+use crate::schema::*; // schemaはすべてインポートしておく
+use chrono::NaiveDateTime;
+use diesel::prelude::*;
 
+#[derive(Queryable, Debug)]
+pub struct Rustacean {
+  pub id: i32,
+  pub name: String,
+  pub email: String,
+  pub created_at: NaiveDateTime, // schema.rs の Timestamp をホバーするといくつか候補になる型を確認できる
+}
+
+#[derive(Insertable, Debug)]
+#[diesel(table_name=rustaceans)] // 新規作成時はrustaceansテーブルに作成するように紐付け
+pub struct NewRustacean {
+  pub name: String,
+  pub email: String,
+}
+
+#[derive(Queryable, Debug)]
+pub struct Crate {
+  pub id: i32,
+  pub rustacean_id: i32,
+  pub code: String,
+  pub name: String,
+  pub version: String,
+  pub description: Option<String>,
+  pub created_at: NaiveDateTime,
+}
+
+#[derive(Insertable, Debug)]
+#[diesel(table_name=crates)] // 新規作成時はcratesテーブルに作成するように紐付け
+pub struct NewCrate {
+  pub rustacean_id: i32,
+  pub code: String,
+  pub name: String,
+  pub version: String,
+  pub description: Option<String>,
+}
