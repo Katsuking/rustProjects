f52aae07c97a036dcdd086b44762b6b09839549a
Katsuking <legendsoccerforte5@gmail.com>
2025-10-17 20:32:59 +0900

    feat: middleware の設定

--- apigateway/src/main6.rs
+++ apigateway/src/main6.rs
@@ -1,23 +1,12 @@
-// Writing Custom Middleware
+// Customizing Headers with Response
 
 use axum::{
   Router,
-  http::{HeaderValue, Request, StatusCode},
-  middleware::{self, Next},
+  http::{HeaderValue, StatusCode},
   response::Response,
   routing,
 };
 use hyper::header;
-
-/// Middleware that ensures every request has an Authorization header.
-async fn require_auth(req: Request<axum::body::Body>, next: Next) -> Result<Response, StatusCode> {
-  if req.headers().get("Authorization").is_some() {
-    println!("req: {:?}", req);
-    Ok(next.run(req).await)
-  } else {
-    Err(StatusCode::UNAUTHORIZED)
-  }
-}
 
 async fn custom_header() -> Response {
   let body = "custom body example";
@@ -31,9 +20,7 @@ async fn custom_header() -> Response {
 
 #[tokio::main]
 async fn main() {
-  let app = Router::new()
-    .route("/custom_header", routing::post(custom_header))
-    .layer(middleware::from_fn(require_auth));
+  let app = Router::new().route("/custom_header", routing::get(custom_header));
   // Define the address and port the server will bind to.
   let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
     .await
