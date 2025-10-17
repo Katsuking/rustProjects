009f42b5d0ae614113ca1d868e9cf4d3a90ea3d1
Katsuking <legendsoccerforte5@gmail.com>
2025-10-17 19:56:39 +0900

    feat: custom header

--- apigateway/src/main.rs
+++ apigateway/src/main.rs
@@ -1,25 +1,26 @@
-// returning JSON response
+// Customizing Headers with Response
 
-use axum::{Json, Router, http::StatusCode, response::IntoResponse, routing};
-use serde::Serialize;
+use axum::{
+  Router,
+  http::{HeaderValue, StatusCode},
+  response::Response,
+  routing,
+};
+use hyper::header;
 
-#[derive(Serialize)]
-struct ApiResponse {
-  message: String,
-  success: bool,
-}
-
-async fn create_user() -> impl IntoResponse {
-  let res = ApiResponse {
-    message: "User created successfully".to_string(),
-    success: true,
-  };
-  (StatusCode::CREATED, Json(res))
+async fn custom_header() -> Response {
+  let body = "custom body example";
+  Response::builder()
+    .status(StatusCode::OK)
+    .header(header::CONTENT_TYPE, "text/plain")
+    .header("X-App-Version", HeaderValue::from_static("1.0"))
+    .body(body.into())
+    .unwrap()
 }
 
 #[tokio::main]
 async fn main() {
-  let app = Router::new().route("/user", routing::get(create_user));
+  let app = Router::new().route("/custom_header", routing::get(custom_header));
   // Define the address and port the server will bind to.
   let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
     .await
