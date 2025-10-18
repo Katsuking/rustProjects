18b30f9b2608fe0a435d6243d80aa2fdf106aa48
Katsuking <legendsoccerforte5@gmail.com>
2025-10-18 23:17:58 +0900

    chore: utils に移動させて 簡略化 反映

--- apigateway/src/main8.rs
+++ apigateway/src/main8.rs
@@ -1,11 +1,11 @@
 // log RUST_LOG=debug cargo run
+mod utils;
 
-use std::sync::{Arc, Mutex};
-
+use crate::utils::init_tracing;
 use axum::{Router, extract::State, http, routing};
+use std::sync::{Arc, Mutex};
 use tower_http::trace::TraceLayer;
 use tracing::info_span;
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[derive(Clone)] // Rustコンパイラが自動的に .clone() メソッドを実装して
 struct AppState {
@@ -17,16 +17,6 @@ async fn increment(State(state): State<AppState>) -> String {
   let mut counter = state.counter.lock().unwrap();
   *counter += 1;
   format!("Counter: {}", *counter)
-}
-
-fn init_tracing() {
-  tracing_subscriber::registry()
-    .with(tracing_subscriber::EnvFilter::new(
-      std::env::var("RUST_LOG")
-        .unwrap_or_else(|_| "axum_api_gateway=debug,tower_http=debug".into()),
-    ))
-    .with(tracing_subscriber::fmt::layer())
-    .init(); // 初期化
 }
 
 #[tokio::main]
